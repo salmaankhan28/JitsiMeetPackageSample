3233ab22bb52c9ffa1257e6161ae3ccd3899a6bc
Rida Sarwar <rida_rocks12@yahoo.com>
2021-07-16 18:03:50 +0500

    react native jitsi sample

---
+++ App.js
@@ -0,0 +1,90 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { useState } from 'react';
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  TouchableOpacity
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import JitsiView from './JitsiView';
+
+const App: () => React$Node = () => {
+  const [joined, setJoinMeeting] = useState(false);
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <View style={styles.body}>
+            {
+              joined ?
+                <JitsiView />
+                :
+                <TouchableOpacity
+                  onPress={() => {
+                    setJoinMeeting(true);
+                  }}
+                >
+                  <Text style={styles.sectionDescription}>
+                    Join Meeting
+             </Text>
+                </TouchableOpacity>
+            }
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+    height: 600
+  },
+  sectionDescription: {
+    marginTop: 30,
+    alignSelf: 'center',
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
---
+++ ios/Pods/Target Support Files/Pods-JitsiMeetPackageSample-tvOS/Pods-JitsiMeetPackageSample-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JitsiMeetPackageSample_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_JitsiMeetPackageSample_tvOS
+@end
